46fa38b95a8bf4ac201e77fe1e1f7c659ce55b48
sunboy3 <example@example.com>
2019-09-23 20:52:53 +0900

    Added c# source code

---
+++ c#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectSample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            {
+            Console.WriteLine("Hello,world!");
+        }
+    }
+}
